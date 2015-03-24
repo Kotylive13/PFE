1b5c96db60ed3abcc3688ed64e31135e8171d779
asmaagit <battouchi.asmaa@gmail.com>
2015-03-25 00:26:01 +0100

    ---:@

--- EPartage/src/main/java/repositories/PublicationDAO.java
+++ EPartage/src/main/java/repositories/PublicationDAO.java
@@ -6,4 +6,10 @@ import domain.Publication;
 
 public interface PublicationDAO extends JpaRepository<Publication, Integer> {
 
+	/*final String PUBLICATIONS_BY_SUBCATEGORY = "SELECT pub FROM Publication pub WHERE pub.subcategory.group.name = :nameG"
+			+ " AND pub.subcategory.category.idCategory.name = :nameC AND pub.subcategory.idSubcategory.subcategory := nameS ";
+
+	@Query(PUBLICATIONS_BY_SUBCATEGORY)
+	public List<Publication> findBySubcategory(@Param("nameG") String nameG,
+			@Param("nameC") String nameC, @Param("nameS") String nameS);*/
 }
