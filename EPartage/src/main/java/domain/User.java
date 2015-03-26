2eafc1166e9bce6c9663a19e7ea6879714419540
dEsk <desk@desk-CX61-2PC.(none)>
2015-03-26 19:09:27 +0100

    Ajout hashcode User

--- EPartage/src/main/java/domain/User.java
+++ EPartage/src/main/java/domain/User.java
@@ -231,17 +231,27 @@ public class User {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (obj == this)
-			return true;
-		
-		if (obj instanceof User) {
-			User user = (User) obj;
-			
-			if (this.id == user.id)
-				return true;
-		}
-		return false;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
